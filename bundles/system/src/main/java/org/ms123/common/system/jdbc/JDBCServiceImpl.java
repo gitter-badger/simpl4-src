a865ea14fc661c1afaaededb863b8e62a7b1f589
ms <manfred@ms123.org>
2015-12-03 17:20:55 +0100

    register AppServer dataSources als osgi service

--- bundles/system/src/main/java/org/ms123/common/system/jdbc/JDBCServiceImpl.java
+++ bundles/system/src/main/java/org/ms123/common/system/jdbc/JDBCServiceImpl.java
@@ -23,13 +23,17 @@ import aQute.bnd.annotation.metatype.*;
 import org.osgi.framework.BundleContext;
 import java.util.Dictionary;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import javax.sql.DataSource;
 import org.osgi.service.jdbc.DataSourceFactory;
+import javax.naming.Context;
+import javax.naming.InitialContext;
 
 @SuppressWarnings("unchecked")
 @Component(enabled = true, configurationPolicy = ConfigurationPolicy.optional, immediate = true, properties = { "rpc.prefix=jdbc" })
-public class JDBCServiceImpl  implements JDBCService {
-
+public class JDBCServiceImpl implements JDBCService {
 
 	private BundleContext m_bundleContext;
 
@@ -40,19 +44,55 @@ public class JDBCServiceImpl  implements JDBCService {
 		try {
 			m_bundleContext = bundleContext;
 			registerAS400();
+			registerJNDIDataSources();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void registerAS400() throws Exception{
+	private void registerAS400() throws Exception {
 		AS400DataSourceFactory dsf = new AS400DataSourceFactory();
 		Dictionary<String, String> props = new Hashtable<String, String>();
 		props.put(DataSourceFactory.OSGI_JDBC_DRIVER_CLASS, "com.ibm.as400.access.AS400JDBCDriver");
 		props.put(DataSourceFactory.OSGI_JDBC_DRIVER_NAME, "as400");
-		System.out.println("as400.register.dsf:"+dsf);
-		System.out.println("as400.register.props:"+props);
+		info("as400.register.dsf:" + dsf);
+		info("as400.register.props:" + props);
 		m_bundleContext.registerService(DataSourceFactory.class.getName(), dsf, props);
+	}
+
+	public synchronized void registerJNDIDataSources() throws Exception {
+		Context ctx = new InitialContext();
+		if (ctx == null) {
+			throw new RuntimeException("WebsphereJNDI could not create InitalContext ");
+		}
+		List<String> jndiNames = new ArrayList();
+		String jndiName = "jdbc/EtkPool";
+		String val = (String) System.getProperty("tpso.jdbc.jndiname");
+		if (val != null) {
+			jndiName = val;
+		}
+		jndiNames.add(jndiName);
+		for (int i = 1; i < 10; i++) {
+			val = (String) System.getProperty("simpl4.jdbc.jndiname" + i);
+			if (val != null) {
+				jndiName = val;
+				jndiNames.add(val);
+			}
+		}
+		for (String name : jndiNames) {
+			info("JDBCServiceImpl.jndiLookup.name:" + name);
+			DataSource datasource = (DataSource) ctx.lookup(name);
+			if (datasource != null) {
+				info("JDBCServiceImpl.DataSource:" + datasource);
+				Dictionary<String, String> props = new Hashtable<String, String>();
+				props.put("dataSourceName", name);
+				m_bundleContext.registerService(DataSource.class.getName(), datasource, props);
+			}
+		}
+	}
+
+	public void info(String msg) {
+		System.out.println(msg);
 	}
 
 	public void update(Map<String, Object> props) {
@@ -61,4 +101,3 @@ public class JDBCServiceImpl  implements JDBCService {
 	protected void deactivate() throws Exception {
 	}
 }
-
