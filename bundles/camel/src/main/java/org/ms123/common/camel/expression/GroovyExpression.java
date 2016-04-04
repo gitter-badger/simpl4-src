e99d8ad20462804434af5cf47e58fbfa62aba939
ms <manfred@ms123.org>
2016-04-04 11:07:04 +0200

    some small correction

--- bundles/camel/src/main/java/org/ms123/common/camel/expression/GroovyExpression.java
+++ bundles/camel/src/main/java/org/ms123/common/camel/expression/GroovyExpression.java
@@ -1,35 +1,37 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * This file is part of SIMPL4(http://simpl4.org).
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * 	Copyright [2014] [Manfred Sattler] <manfred@ms123.org>
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SIMPL4 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SIMPL4 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SIMPL4.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.ms123.common.camel.expression;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import groovy.lang.Script;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import org.apache.camel.Exchange;
-import org.apache.camel.Message;
 import org.apache.camel.ExchangePattern;
+import org.apache.camel.language.groovy.GroovyShellFactory;
+import org.apache.camel.Message;
 import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.support.ExpressionSupport;
 import org.apache.camel.util.ExchangeHelper;
-import org.apache.camel.language.groovy.GroovyShellFactory;
 import org.apache.camel.util.LRUSoftCache;
 
 /**
