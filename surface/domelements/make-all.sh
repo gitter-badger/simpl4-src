06847fa9c54522e331455379d2d361b6673b9790
ms <manfred@ms123.org>
2016-04-16 05:33:37 +0200

    removed expandjs stuff

--- surface/domelements/make-all.sh
+++ surface/domelements/make-all.sh
@@ -10,7 +10,6 @@ if [ ! -d "bower_components" ] ; then
 		 
 		 sed -i '/slide-up-scale-down-animation.html/d'  bower_components/neon-animation/neon-animations.html
 		 sed -i 's!value: Polymer.IronOverlayManager!value: function(){ return Polymer.IronOverlayManager}!' bower_components/iron-overlay-behavior/*.html
-		 sed -i '/    html {/,+14d'  bower_components/mat-typography/mat-typography.html
 		 sed -e '/target.insertBefore/ {' -e 'r polymer.patch' -e 'd' -e '}' -i bower_components/polymer/polymer.html
 		 sed -i '/configure selectedPage animations/a this.animationConfig = [];'  bower_components/neon-animation/neon-animated-pages.html
 
