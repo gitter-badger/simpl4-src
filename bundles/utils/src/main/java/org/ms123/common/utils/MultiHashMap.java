921a5dfefa0f8abd393e9dca266cb750b76a00cc
ms <manfred@ms123.org>
2016-05-29 02:08:14 +0200

    initial checkin

---
+++ bundles/utils/src/main/java/org/ms123/common/utils/MultiHashMap.java
@@ -0,0 +1,145 @@
+/**
+ * This file is part of SIMPL4(http://simpl4.org).
+ *
+ * 	Copyright [2014] [Manfred Sattler] <manfred@ms123.org>
+ *
+ * SIMPL4 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SIMPL4 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SIMPL4.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.ms123.common.utils;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.List;
+
+/**
+ * A MultiHashMap is a HashMap capable of holding multiple objects per key. 
+ * If a key has only one object mapped to it, get returns that object; 
+ * if the key has multiple objects mapped to it, get returns a list. 
+ * The getArray method returns an array in all cases; 
+ * if the key has only object mapped to it, the length of the array will be 1.
+ */
+
+@SuppressWarnings("unchecked")
+public class MultiHashMap extends HashMap {
+
+	//
+	// constructors
+	//
+	public MultiHashMap() {
+		super();
+	}
+
+	public MultiHashMap(int nInitialCapacity) {
+		super(nInitialCapacity);
+	}
+
+	public MultiHashMap(int nInitialCapacity, float fLoadFactor) {
+		super(nInitialCapacity, fLoadFactor);
+	}
+
+	public MultiHashMap(Map oMap) {
+		super(oMap);
+	}
+
+	//
+	// methods
+	//
+
+	/**
+	 * Puts a new element into the HashMap.
+	 * If the given key already exists in the HashMap
+	 * the new element will be assign to that key as well.
+	 * A linked list is used to store all elements
+	 * assign to a key.
+	 * If the new element is an instance of a collection
+	 * class all elements of the collection will be assigned
+	 * to the key.
+	 */
+	public Object put(Object oKey, Object oNewElement) {
+		LinkedList oElements;
+
+		if (containsKey(oKey) == true) {
+			oElements = (LinkedList) super.get(oKey);
+		} else {
+			oElements = new LinkedList();
+		}
+
+		if (oNewElement instanceof Collection) {
+			oElements.addAll((Collection) oNewElement);
+		} else {
+			oElements.add(oNewElement);
+		}
+
+		super.put(oKey, oElements);
+
+		return null;
+	}
+
+	/**
+	 * Returns the value or values to which this map maps the specified key.
+	 */
+	public Object get(Object key) {
+		List list = (List) super.get(key);
+		if (list == null)
+			return null;
+
+		if (list.size() == 1) {
+			return list.get(0);
+		}
+		return list;
+	}
+
+	/**
+	 * Returns the values to which this map maps the specified key.
+	 */
+	public Object getList(Object key) {
+		List list = (List) super.get(key);
+		if (list == null)
+			return new java.util.ArrayList();
+		return list;
+	}
+
+	/**
+	 * Returns the number of values mapped to a key.
+	 */
+	public int getValueCount(Object key) {
+		List list = (List) super.get(key);
+		if (list == null)
+			return 0;
+		return list.size();
+	}
+
+	/**
+	 * Returns the value or values to which this map maps the specified key.
+	 */
+	public Object[] getArray(Object key, Object[] a) {
+		List list = (List) super.get(key);
+		if (list == null) {
+			return (Object[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), 0);
+		}
+		return list.toArray(a);
+	}
+
+	/**
+	 * Returns the value or values to which this map maps the specified key.
+	 */
+	public Object[] getArray(Object key) {
+		List list = (List) super.get(key);
+		if (list == null)
+			return new Object[0];
+		return list.toArray();
+	}
+}
+
