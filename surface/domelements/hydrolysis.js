bba262cab4f51b6485dc2440fbbe9352bb33d265
ms <manfred@ms123.org>
2016-08-12 16:44:53 +0200

    prepared for documentations purposes

--- surface/domelements/hydrolysis.js
+++ surface/domelements/hydrolysis.js
@@ -1,12 +1,14 @@
 var hydrolysis = require( 'hydrolysis' );
+//var escape = require('html-escape');
 
 var showList = [];
-showList.push("simpl-websocket");
-showList.push("simpl-rpc");
-showList.push("simpl-datatables");
-showList.push("simpl-form");
+showList.push( "simpl-websocket" );
+showList.push( "simpl-rpc" );
+showList.push( "simpl-datatables" );
+showList.push( "simpl-form" );
 
-//hydrolysis.Analyzer.analyze( '/opt/dev/simpl4-src/surface/domelements/bower_components/iron-collapse/iron-collapse.html' )
+runHydrolysis();
+/*
 hydrolysis.Analyzer.analyze( 'docindex.html' )
 	.then( function( analyzer ) {
 		try {
@@ -28,7 +30,7 @@ hydrolysis.Analyzer.analyze( 'docindex.html' )
 
 
 function removeProps( obj, keys, level ) {
-	if ( level > 40 ) return;
+	if ( level > 400 ) return;
 	if ( obj instanceof Array ) {
 		obj.forEach( function( item ) {
 			removeProps( item, keys, level++ )
@@ -42,4 +44,51 @@ function removeProps( obj, keys, level ) {
 			}
 		} );
 	}
+}
+*/
+
+function runHydrolysis() {
+	hydrolysis.Analyzer.analyze( 'docindex.html' )
+		.then( function( analyzer ) {
+			var sections = analyzer.elements;
+			sections = sections.concat( analyzer.behaviors );
+
+			for ( var i = 0; i < sections.length; i++ ) {
+				var sectionName = sections[ i ][ 'is' ];
+				var section = cleanUpSectionDocs( sections[ i ] );
+				if ( showList.indexOf( section.is ) == -1 ) {
+					section.show = false;
+				} else {
+					section.show = true;
+				}
+
+			}
+
+
+			var elemNames = Object.keys( analyzer.elementsByTagName );
+			for ( var i = 0; i < elemNames.length; i++ ) {
+				var name = elemNames[ i ];
+				var elem = analyzer.elementsByTagName[ name ];
+				cleanUpSectionDocs( elem );
+			}
+			var json = JSON.stringify( {
+				elements: sections,
+				elementsByTagName: analyzer.elementsByTagName,
+				behaviors: []
+			}, null, 2 );
+			console.log( json )
+		}, function( e ) {
+			console.log( 'Could not run hydrolysis', e );
+		} );
+}
+
+function cleanUpSectionDocs( section ) {
+	section.scriptElement = undefined;
+	section.behaviors && section.behaviors.forEach( function( behavior ) {
+		behavior.javascriptNode = undefined;
+	} );
+	section.properties && section.properties.forEach( function( property ) {
+		property.javascriptNode = undefined;
+	} );
+	return section;
 }
