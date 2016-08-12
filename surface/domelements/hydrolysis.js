023281a8343c806c827d0dda8d6749ce510d2d85
ms <manfred@ms123.org>
2016-08-12 02:54:53 +0200

    mechanism for not displaying elements

--- surface/domelements/hydrolysis.js
+++ surface/domelements/hydrolysis.js
@@ -1,10 +1,25 @@
 var hydrolysis = require( 'hydrolysis' );
 
+var showList = [];
+showList.push("simpl-websocket");
+showList.push("simpl-rpc");
+showList.push("simpl-datatables");
+showList.push("simpl-form");
+
 //hydrolysis.Analyzer.analyze( '/opt/dev/simpl4-src/surface/domelements/bower_components/iron-collapse/iron-collapse.html' )
 hydrolysis.Analyzer.analyze( 'docindex.html' )
 	.then( function( analyzer ) {
 		try {
 			removeProps( analyzer, [ 'javascriptNode', 'scriptElement', 'html', 'parsedDocuments', 'parsedScripts', "_content", 'loader', 'contentHref', 'observerNode' ], 0 );
+
+			for( var i=0; i< analyzer.elements.length; i++){
+				var e = analyzer.elements[i];
+				if( showList.indexOf(e.is) == -1){
+					e.show = false;
+				}else{
+					e.show = true;
+				}
+			}
 			console.log( JSON.stringify( analyzer, null, 2 ) )
 		} catch ( e ) {
 			console.log( e.stack );
