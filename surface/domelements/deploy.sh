4a0e7ab9f06e1def01a089ddcf442bc002c347d3
ms <manfred@ms123.org>
2017-07-02 21:00:57 +0200

    domelements.html and domelements.js now separate

--- surface/domelements/deploy.sh
+++ surface/domelements/deploy.sh
@@ -2,6 +2,7 @@
 make-all.sh
 #html-beautify -r domelements.html
 gzip -f domelements.html
+gzip -f domelements.js
 #scp domelements.html.gz  ms:$simpl4first/client/surface/
-scp domelements.html.gz  vz5:$wawidev/client/surface/
-#scp elements.html.gz  ms:$web/client/surface/
+#scp domelements.html.gz  vz5:$wawidev/client/surface/
+scp domelements.html.gz domelements.js.gz vz5:$wawidev/client/surface/
