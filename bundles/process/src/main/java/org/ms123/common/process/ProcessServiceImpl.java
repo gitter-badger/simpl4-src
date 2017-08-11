2a9e70355bf646fa31baa3dde555c36a5b69e4ae
ms <manfred@ms123.org>
2017-08-12 01:53:33 +0200

    camunda process initial checkin

---
+++ bundles/process/src/main/java/org/ms123/common/process/ProcessServiceImpl.java
@@ -0,0 +1,83 @@
+/**
+ * This file is part of SIMPL4(http://simpl4.org).
+ *
+ * 	Copyright [2014-2017] [Manfred Sattler] <manfred@ms123.org>
+ *
+ * SIMPL4 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SIMPL4 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SIMPL4.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.ms123.common.process;
+
+import aQute.bnd.annotation.component.*;
+import aQute.bnd.annotation.metatype.*;
+import flexjson.JSONDeserializer;
+import flexjson.JSONSerializer;
+import java.io.*;
+import java.util.*;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.ms123.common.rpc.PDefaultBool;
+import org.ms123.common.rpc.PDefaultFloat;
+import org.ms123.common.rpc.PDefaultInt;
+import org.ms123.common.rpc.PDefaultLong;
+import org.ms123.common.rpc.PDefaultString;
+import org.ms123.common.rpc.PName;
+import org.ms123.common.rpc.POptional;
+import org.ms123.common.rpc.RpcException;
+import org.ms123.common.permission.api.PermissionService;
+import org.osgi.framework.BundleContext;
+import static org.ms123.common.rpc.JsonRpcServlet.ERROR_FROM_METHOD;
+import static org.ms123.common.rpc.JsonRpcServlet.INTERNAL_SERVER_ERROR;
+import static org.ms123.common.rpc.JsonRpcServlet.PERMISSION_DENIED;
+
+/** ProcessService implementation
+ */
+@Component(enabled = true, configurationPolicy = ConfigurationPolicy.optional, immediate = true, properties = { "rpc.prefix=process" })
+public class ProcessServiceImpl extends BaseProcessServiceImpl implements ProcessService {
+	private static final String NAME = "name";
+	private static final String NAMESPACE = "namespace";
+
+	private PermissionService permissionService;
+
+	public ProcessServiceImpl() {
+		this.js.prettyPrint(true);
+	}
+
+	protected void activate(BundleContext bundleContext, Map<?, ?> props) {
+		this.bc = bundleContext;
+	}
+
+	protected void deactivate() throws Exception {
+		System.out.println("ProcessServiceImpl deactivate");
+	}
+
+	/* BEGIN JSON-RPC-API*/
+//	@RequiresRoles("admin")
+	public Map deployDMN(
+			@PName(NAMESPACE)               String namespace, 
+			@PName(NAME)                    String name, 
+			@PName("jsonString") @POptional String jsonString) throws RpcException {
+		try {
+			return null;
+		} catch (Throwable e) {
+			throw new RpcException(ERROR_FROM_METHOD, INTERNAL_SERVER_ERROR, "ProcessServiceImpl.deployDMN:", e);
+		} finally {
+		}
+	}
+
+	/* END JSON-RPC-API*/
+	@Reference(dynamic = true, optional = true)
+	public void setPermissionService(PermissionService permissionService) {
+		this.permissionService = permissionService;
+	}
+}
