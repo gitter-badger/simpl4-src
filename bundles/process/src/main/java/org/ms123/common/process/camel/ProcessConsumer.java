d86f872404073907f35cb7027853b4f6d96b2b4a
ms <manfred@ms123.org>
2017-08-24 18:58:02 +0200

    .

--- bundles/process/src/main/java/org/ms123/common/process/camel/ProcessConsumer.java
+++ bundles/process/src/main/java/org/ms123/common/process/camel/ProcessConsumer.java
@@ -30,6 +30,7 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.HashSet;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import org.ms123.common.system.thread.ThreadContext;
@@ -52,7 +53,7 @@ public class ProcessConsumer extends DefaultConsumer implements EventHandler {
 	private ProcessEndpoint endpoint;
 	private ServiceRegistration serviceRegistration;
 	private String tenant;
-	private String events[];
+	private String wantedEvents[];
 
 	public ProcessConsumer(ProcessEndpoint endpoint, Processor processor) {
 		super(endpoint, processor);
@@ -60,25 +61,57 @@ public class ProcessConsumer extends DefaultConsumer implements EventHandler {
 		String _events = this.endpoint.getEvents();
 		if (_events != null && _events.trim().length() > 0) {
 			info(this, "_Events:" + _events);
-			this.events = _events.toUpperCase().split(",");
+			this.wantedEvents = _events.split(",");
 		}
 	}
 
 	public boolean isFailOnException() {
 		return false;
 	}
+	private boolean isEventWanted( String topic, String eventName){
+		for( String we : this.wantedEvents){
+			String en = we.split( "_")[1];
+			if( en.regionMatches(0, eventName, 0, 5) ){
+				return true;
+			}
+		}
+		return false;
+	}
 
 	public void handleEvent(Event event) {
-		info(this, "HandleEvent.onEvent " + event);
+		String eventName = (String) event.getProperty("eventName");
+		String topic = event.getTopic();
+		info(this, "HandleEvent.onEvent("+topic+","+eventName+")");
+
+		if( !isEventWanted( topic, eventName)){
+			info(this,"Event("+topic+","+eventName+") not wanted");
+			return;
+		}
+		info(this,"Event("+topic+","+eventName+") wanted");
+
 		String[] propertyNames = event.getPropertyNames();
+		Map<String,Object> properties = new HashMap<String,Object>();
 		for (String name : propertyNames) {
 			info(this, " - " + name + ": " + event.getProperty(name));
+			properties.put( name, event.getProperty(name));
 		}
-		String type = (String) event.getProperty("type");
-		if (event.getTopic().startsWith(TASK_EVENT_TOPIC)) {
-		} else if (event.getTopic().startsWith(EXECUTION_EVENT_TOPIC)) {
-		} else if (event.getTopic().startsWith(VARIABLE_EVENT_TOPIC)) {
-		} else if (event.getTopic().startsWith(PROCESS_EVENT_TOPIC)) {
+
+		if (topic.startsWith(TASK_EVENT_TOPIC)) {
+		} else if (topic.startsWith(EXECUTION_EVENT_TOPIC)) {
+		} else if (topic.startsWith(VARIABLE_EVENT_TOPIC)) {
+		} else if (topic.startsWith(PROCESS_EVENT_TOPIC)) {
+		}
+		info(this, "ProcessConsumer.onEvent.properties:"+ properties);
+		final Exchange exchange = endpoint.createExchange(ExchangePattern.InOnly);
+		exchange.getIn().setBody(properties);
+		try {
+			getAsyncProcessor().process(exchange, new AsyncCallback() {
+				@Override
+				public void done(boolean doneSync) {
+				}
+			});
+		} catch (Exception e) {
+			getExceptionHandler().handleException("ProcessConsumer.Error processing HandleEvent("+topic+","+eventName+")", exchange, e);
 		}
 
 	}
@@ -86,12 +119,22 @@ public class ProcessConsumer extends DefaultConsumer implements EventHandler {
 	@Override
 	protected void doStart() throws Exception {
 		this.tenant = ThreadContext.getThreadContext().getUserName();
-		info(this, "Registering EventHandler handler fore tenant:"+this.tenant);
-		String[] topics = new String[] { TASK_EVENT_TOPIC + "/" +this.tenant, 
-																			EXECUTION_EVENT_TOPIC + "/"+ this.tenant,
-																			VARIABLE_EVENT_TOPIC + "/"+ this.tenant,
-																			PROCESS_EVENT_TOPIC + "/"+ this.tenant 
-															};
+		Set<String> topicList = new HashSet<String>();
+		for( String we : this.wantedEvents){
+			info(this,"wantedEvent:"+we);
+			if (we.startsWith("task_")) {
+				topicList.add( TASK_EVENT_TOPIC + "/" +this.tenant);
+			} else if (we.startsWith("activity_")) {
+				topicList.add( EXECUTION_EVENT_TOPIC + "/" +this.tenant);
+			} else if (we.startsWith("process_")) {
+				topicList.add( PROCESS_EVENT_TOPIC + "/" +this.tenant);
+			} else if (we.startsWith("variable_")) {
+				topicList.add( VARIABLE_EVENT_TOPIC + "/" +this.tenant);
+			}
+		}
+
+		info(this, "Registering EventHandler handler fore tenant("+this.tenant+"):"+topicList);
+		String[] topics = topicList.toArray(new String[topicList.size()]);
 		Dictionary dict = new Hashtable();
 		dict.put(EventConstants.EVENT_TOPIC, topics);
 		this.serviceRegistration = this.endpoint.getBundleContext().registerService(EventHandler.class.getName(), this, dict);
