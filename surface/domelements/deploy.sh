b06d6eafa11adc2fefc27852d777d876e73fc4f4
ms <manfred@ms123.org>
2017-08-25 12:27:22 +0200

    some work in the context of camuda engine

--- surface/domelements/deploy.sh
+++ surface/domelements/deploy.sh
@@ -5,4 +5,4 @@ gzip -f domelements.html
 gzip -f domelements.js
 #scp domelements.html.gz  ms:$simpl4first/client/surface/
 #scp domelements.html.gz  vz5:$wawidev/client/surface/
-scp domelements.html.gz domelements.js.gz vz5:$wawidev/client/surface/
+scp domelements.html.gz domelements.js.gz vz5:$flowabledev/client/surface/
