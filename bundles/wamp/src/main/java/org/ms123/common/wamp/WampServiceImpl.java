0c4d008a412d31241aa25d93cfed08a5eb2f6dd8
ms <manfred@ms123.org>
2017-09-08 22:52:50 +0200

    forgotten to checkin

---
+++ bundles/wamp/src/main/java/org/ms123/common/wamp/WampServiceImpl.java
@@ -0,0 +1,329 @@
+/**
+ * This file is part of SIMPL4(http://simpl4.org).
+ *
+ * 	Copyright [2017] [Manfred Sattler] <manfred@ms123.org>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ms123.common.wamp;
+
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.ConfigurationPolicy;
+import aQute.bnd.annotation.component.Reference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketListener;
+import org.ms123.common.rpc.JsonRpc;
+import org.ms123.common.rpc.PName;
+import org.ms123.common.rpc.RpcException;
+import org.ms123.common.wamp.WampMessages.*;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rx.exceptions.OnErrorThrowable;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.Observable;
+import rx.Observable.OnSubscribe;
+import rx.Observer;
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
+import rx.subjects.AsyncSubject;
+import rx.subjects.BehaviorSubject;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+import rx.subscriptions.Subscriptions;
+import static org.ms123.common.rpc.JsonRpcServlet.ERROR_FROM_METHOD;
+import static org.ms123.common.rpc.JsonRpcServlet.INTERNAL_SERVER_ERROR;
+import static org.ms123.common.rpc.JsonRpcServlet.PERMISSION_DENIED;
+import org.ms123.common.system.thread.*;
+
+/** WampService implementation
+ */
+@SuppressWarnings("unchecked")
+@Component(enabled = true, configurationPolicy = ConfigurationPolicy.optional, immediate = true, properties = { "rpc.prefix=wamp" })
+public class WampServiceImpl extends BaseWampServiceImpl implements WampService {
+
+	private static final Logger m_logger = LoggerFactory.getLogger(WampServiceImpl.class);
+
+	private static Map<String, Realm> m_realms;
+	public static String DEFAULT_REALM = "default";
+
+	private List<String> m_registeredMethodList = new ArrayList();
+	private Map<Long, Procedure> m_registeredMethodMap = new HashMap();
+
+	private WampRouterSession m_localWampRouterSession;
+	private ObjectMapper m_objectMapper = new ObjectMapper();
+	private JsonRpc m_jsonRpc;
+
+	public WampServiceImpl() {
+	}
+
+
+	protected void activate(BundleContext bundleContext, Map<?, ?> props) {
+		m_jsonRpc = new JsonRpc(bundleContext);
+		registerMethods();
+	}
+
+	protected void deactivate() throws Exception {
+		for (Realm realm : getRealms().values()) {
+			for (WampRouterSession.SessionContext context : realm.m_contextList) {
+				realm.removeSession(context, false);
+				String goodbye = WampCodec.encode(new GoodbyeMessage(null, ApplicationError.SYSTEM_SHUTDOWN));
+				if( context.webSocket != null){
+					context.webSocket.sendStringByFuture(goodbye);
+				}
+			}
+			realm.m_contextList.clear();
+		}
+	}
+
+	//@RequiresRoles("admin")
+	private void registerMethods() {
+/* $if version >= 1.8 $ */
+		List<String> methodList = new ArrayList();
+		methodList.add("enumeration.get");
+		methodList.add("data.query");
+		if (m_localWampRouterSession == null) {
+			BaseWebSocket dummyWebSocket = new BaseWebSocket() {
+
+				public void sendStringByFuture(String message) {
+					ExecutorService executor = Executors.newSingleThreadExecutor();
+					executor.submit(() -> {
+						WampMessage msg = WampCodec.decode(message.getBytes());
+						info("Local.sendStringByFuture:" + msg);
+						if (msg instanceof RegisteredMessage) {
+							RegisteredMessage regMsg = (RegisteredMessage) msg;
+							Procedure proc = new Procedure(m_registeredMethodList.get((int) regMsg.requestId), null, regMsg.registrationId);
+							m_registeredMethodMap.put(regMsg.registrationId, proc);
+						} else if (msg instanceof WelcomeMessage) {
+							doRegisterMethods(methodList);
+						} else if (msg instanceof InvocationMessage) {
+							InvocationMessage invMsg = (InvocationMessage) msg;
+							Procedure proc = m_registeredMethodMap.get(invMsg.registrationId);
+							info("Invocation:" + proc.procName + "/" + invMsg.arguments + "/" + invMsg.argumentsKw + "/ThreadId" + Thread.currentThread().getId());
+
+							String paramString = invMsg.argumentsKw != null ? invMsg.argumentsKw.toString() : "";
+							Map<String, Object> result = m_jsonRpc.handleRPC(proc.procName, paramString);
+							Object error = result.get("error");
+							if (error != null) {
+								String errMsg = WampCodec.encode(new ErrorMessage(InvocationMessage.ID, invMsg.requestId, null, result.toString(), null, null));
+								m_localWampRouterSession.onWebSocketText(errMsg);
+							} else {
+								ArrayNode resultNode = m_objectMapper.createArrayNode();
+								resultNode.add((JsonNode) m_objectMapper.valueToTree(result));
+								String yield = WampCodec.encode(new YieldMessage(invMsg.requestId, null, resultNode, null));
+								m_localWampRouterSession.onWebSocketText(yield);
+							}
+						}
+					});
+				}
+			};
+			m_localWampRouterSession = new WampRouterSession(dummyWebSocket, getRealms());
+			m_localWampRouterSession.onWebSocketConnect(null);
+			m_localWampRouterSession.onWebSocketText(WampCodec.encode(new HelloMessage(DEFAULT_REALM, null)));
+		} else {
+			doRegisterMethods(methodList);
+		}
+/* $endif$ */
+	}
+
+	private void doRegisterMethods(List<String> methodList) {
+		long i = m_registeredMethodList.size();
+		for (String meth : methodList) {
+			if (m_registeredMethodList.contains(meth)) {
+				continue;
+			}
+			m_registeredMethodList.add(meth);
+			String register = WampCodec.encode(new RegisterMessage(i++, null, meth));
+			m_localWampRouterSession.onWebSocketText(register);
+		}
+	}
+
+	@RequiresRoles("admin")
+	public void start() throws RpcException {
+/* $if version >= 1.8 $ */
+		final WampClientSession client1 = createWampClientSession(DEFAULT_REALM);
+		client1.statusChanged().subscribe((t1) -> {
+			System.out.println("Session1 status changed to " + t1);
+			if (t1 == WampClientSession.Status.Connected) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+				}
+
+				Subscription addProcSubscription= client1.registerProcedure("com.myapp.add2").subscribe((request) -> {
+					if (request.arguments() == null || request.arguments().size() != 2 || !request.arguments().get(0).canConvertToLong() || !request.arguments().get(1).canConvertToLong()) {
+						try {
+							request.replyError(new ApplicationError(ApplicationError.INVALID_PARAMETER));
+						} catch (ApplicationError e) {
+							e.printStackTrace();
+						}
+					} else {
+						long a = request.arguments().get(0).asLong();
+						long b = request.arguments().get(1).asLong();
+						request.reply(a + b);
+					}
+				});
+
+				//							 client1.registerProcedure("enumeration.get").subscribe();
+
+				Observable<Long> result1 = client1.call("com.myapp.add2", Long.class, 33, 66);
+				result1.subscribe((t2) -> {
+					System.out.println("Completed add with result " + t2);
+				}, (t3) -> {
+					System.out.println("Completed add with error " + t3);
+				});
+
+			}
+		}, (t) ->  {
+				System.out.println("Session1 ended with error " + t);
+		}, ()-> {
+				System.out.println("Session1 ended normally");
+		});
+
+		//client1.open();
+/* $endif$ */
+	}
+	private static Map<String, Realm> getRealms (){
+		if( m_realms == null){
+			m_realms = new HashMap();
+			Set<WampRoles> roles = new HashSet();
+			roles.add(WampRoles.Broker);
+			RealmConfig realmConfig = new RealmConfig(roles, false);
+			m_realms.put(DEFAULT_REALM, new Realm(realmConfig));
+		}
+		return m_realms;
+	}
+
+	public static WampClientSession createWampClientSession(String realm) {
+		WampClientWebSocket ws = new WampClientWebSocket();
+		WampClientSession wcs = new WampClientSession(ws, realm, getRealms());
+		ws.setWampClientSession(wcs);
+		return wcs;
+	}
+
+	public static class WampClientWebSocket extends BaseWebSocket {
+		private WampClientSession m_wampClientSession;
+		private WampRouterSession m_wampRouterSession;
+
+		public WampClientWebSocket() {
+		}
+
+		public void setWampClientSession(WampClientSession wcs) {
+			m_wampClientSession = wcs;
+		}
+
+		public void setWampRouterSession(WampRouterSession wrs) {
+			m_wampRouterSession = wrs;
+		}
+
+		public void sendStringByFuture(String message) {
+/* $if version >= 1.8 $ */
+			info("WampClientWebSocket.sendStringByFuture:" + message);
+			ExecutorService executor = Executors.newSingleThreadExecutor();
+			executor.submit(() -> {
+				m_wampClientSession.onWebSocketText(message);
+			});
+/* $endif$ */
+		}
+		@Override
+		public void onWebSocketConnect(Session sess) {
+			info("WampClientWebSocket.onWebSocketConnect");
+			m_wampRouterSession.onWebSocketConnect(sess);
+		}
+
+		@Override
+		public void onWebSocketText(String message) {
+			m_wampRouterSession.onWebSocketText(message);
+		}
+		@Override
+		public void onWebSocketClose(int status, String reason) {
+/* $if version >= 1.8 $ */
+			info("WampClientWebSocket.onWebSocketClose");
+			ExecutorService executor = Executors.newSingleThreadExecutor();
+			executor.submit(() -> {
+				m_wampRouterSession.onWebSocketClose(status,reason);
+			});
+/* $endif$ */
+		}
+	}
+
+	public WebSocketListener createWebSocket(Map<String, Object> config, Map<String, String> parameterMap) {
+		return new WampRouterWebSocket(config, parameterMap);
+	}
+
+	public class WampRouterWebSocket extends BaseWebSocket {
+		private Map<String, Object> m_config = null;
+		private Map<String, String> m_params;
+		private WampRouterSession m_wampRouterSession;
+		private ThreadContext threadContext;
+		private  Map<Object, Object>  shiroResources;
+
+		public WampRouterWebSocket(Map<String, Object> config, Map<String, String> parameterMap) {
+			m_config = config;
+			m_params = parameterMap;
+			String namespace = m_params.get("namespace");
+			String routesName = m_params.get("routes");
+			m_wampRouterSession = new WampRouterSession(this, getRealms());
+			debug("WampRouterWebSocket.currentThread:" + Thread.currentThread().getName());
+			this.threadContext = ThreadContext.getThreadContext();
+			shiroResources = org.apache.shiro.util.ThreadContext.getResources();
+		}
+
+		@Override
+		public void onWebSocketConnect(Session sess) {
+			super.onWebSocketConnect(sess);
+			m_wampRouterSession.onWebSocketConnect(sess);
+		}
+
+		@Override
+		public void onWebSocketText(String message) {
+			ThreadContext.loadThreadContext(this.threadContext);
+			org.apache.shiro.util.ThreadContext.setResources(shiroResources);
+			m_wampRouterSession.onWebSocketText(message);
+		}
+
+		@Override
+		public void onWebSocketBinary(byte[] payload, int offset, int len) {
+			m_wampRouterSession.onWebSocketBinary(payload, offset, len);
+		}
+
+		@Override
+		public void onWebSocketClose(int statusCode, String reason) {
+			super.onWebSocketClose(statusCode, reason);
+			m_wampRouterSession.onWebSocketClose(statusCode, reason);
+		}
+
+		@Override
+		public void onWebSocketError(Throwable cause) {
+			m_wampRouterSession.onWebSocketError(cause);
+		}
+	}
+}
+
---
+++ bundles/wamp/src/main/java/org/ms123/common/wamp/camel/WampClientConsumer.java
@@ -0,0 +1,414 @@
+/**
+ * This file is part of SIMPL4(http://simpl4.org).
+ *
+ * 	Copyright [2017] [Manfred Sattler] <manfred@ms123.org>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ms123.common.wamp.camel;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.camel.AsyncCallback;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.impl.DefaultConsumer;
+import org.apache.camel.Processor;
+import org.apache.camel.util.ExchangeHelper;
+import org.apache.commons.beanutils.ConvertUtils;
+import org.ms123.common.permission.api.PermissionService;
+import org.ms123.common.system.thread.ThreadContext;
+import org.ms123.common.wamp.ApplicationError;
+import org.ms123.common.wamp.Request;
+import org.ms123.common.wamp.PubSubData;
+import org.ms123.common.wamp.WampClientSession;
+import rx.Subscription;
+import static org.ms123.common.wamp.camel.WampClientConstants.*;
+import static com.jcabi.log.Logger.info;
+import static com.jcabi.log.Logger.debug;
+import static com.jcabi.log.Logger.error;
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class WampClientConsumer extends DefaultConsumer {
+
+	private final WampClientEndpoint endpoint;
+	private WampClientSession clientSession;
+	private Processor processor;
+	private PermissionService permissionService;
+	private ObjectMapper objectMapper = new ObjectMapper();
+	protected static final Map<String, Class> types = new HashMap<String, Class>() {
+
+		{
+			put("string", java.lang.String.class);
+			put("integer", java.lang.Integer.class);
+			put("double", java.lang.Double.class);
+			put("boolean", java.lang.Boolean.class);
+			put("map", java.util.Map.class);
+			put("list", java.util.List.class);
+		}
+	};
+
+	public WampClientConsumer(WampClientEndpoint endpoint, Processor processor) {
+		super(endpoint, processor);
+		this.endpoint = endpoint;
+		this.processor = processor;
+	}
+
+	private void wampClientConnected() {
+/* $if version >= 1.8 $ */
+		String namespace = endpoint.getCamelContext().getName().split("/")[0];
+		String fqPrefix = endpoint.getCamelContext().getName().replace("/",".");
+		info(this,"Consumer.register:" + fqPrefix + "." + endpoint.getProcedure()+"|fqPrefix:"+fqPrefix);
+		String mode = endpoint.getMode();
+		if( "rpc".equals(mode)){
+			Subscription addProcSubscription = this.clientSession.registerProcedure(fqPrefix + "." + endpoint.getProcedure()).subscribe((request) -> {
+
+				info(this,"Consumer.Procedure called:" + request + "/hashCode:" + this.hashCode());
+				final boolean reply = false;
+				final Exchange exchange = endpoint.createExchange(reply ? ExchangePattern.InOut : ExchangePattern.InOnly);
+				try {
+					prepareExchange(exchange, request);
+				} catch (Exception e) {
+					try {
+						request.replyError("X", buildErrorResponse(e));
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+					return;
+				}
+				try {
+					getAsyncProcessor().process(exchange, new AsyncCallback() {
+
+						@Override
+						public void done(boolean doneSync) {
+							if (exchange.getException() != null) {
+								try {
+									request.replyError("XXXX", buildErrorResponse(exchange.getException()));
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							} else {
+								request.reply(null, buildResponse(getResult(exchange)));
+							}
+						}
+					});
+				} catch (Exception e) {
+					e.printStackTrace();
+					error(this, "process.error:%[exception]s",e);
+				}
+			});
+		}
+		if( "subscribe".equals(mode)){
+			Subscription addSubscription = this.clientSession.makeSubscription(namespace + "." + endpoint.getTopic2()).subscribe((request) -> {
+
+				info(this,"Consumer.Subscribe called:" + request + "/hashCode:" + this.hashCode());
+				final boolean reply = false;
+				final Exchange exchange = endpoint.createExchange(reply ? ExchangePattern.InOut : ExchangePattern.InOnly);
+				try {
+					prepareExchange(exchange, request);
+				} catch (Exception e) {
+					error(this, "prepareExchange.error:%[exception]s",e);
+					return;
+				}
+				try {
+					getAsyncProcessor().process(exchange, new AsyncCallback() {
+
+						@Override
+						public void done(boolean doneSync) {
+							if (exchange.getException() != null) {
+								error(this, "done.error:%[exception]s",exchange.getException());
+							}
+						}
+					});
+				} catch (Exception e) {
+					error(this, "process.error:%[exception]s",e);
+
+				}
+			});
+		}
+/* $endif$ */
+	}
+
+	private void prepareExchange(Exchange exchange, Request request) {
+		if (this.permissionService == null) {
+			this.permissionService = getByType(exchange.getContext(), PermissionService.class);
+		}
+		Map<String, Object> methodParams = objectMapper.convertValue(request.keywordArguments(), Map.class);
+		List<String> permittedRoleList = this.endpoint.getPermittedRoles();
+		List<String> permittedUserList = this.endpoint.getPermittedUsers();
+		String userName = getUserName();
+		List<String> userRoleList = getUserRoles(userName);
+		debug(this,"Consumer.prepare.userName:" + userName);
+		debug(this,"Consumer.prepare.userRoleList:" + userRoleList);
+		debug(this,"Consumer.prepare.permittedRoleList:" + permittedRoleList);
+		debug(this,"Consumer.prepare.permittedUserList:" + permittedUserList);
+		if (!isPermitted(userName, userRoleList, permittedUserList, permittedRoleList)) {
+			throw new RuntimeException(PERMISSION_DENIED + ":User(" + userName + ") has no permission");
+		}
+
+		Map<String, Object> properties = new HashMap<>();
+		Map<String, Object> headers = new HashMap<>();
+		Map<String, Object> bodyMap = new HashMap<>();
+		Object bodyObj = null;
+		List<Map> paramList = this.endpoint.getParamList();
+		int bodyCount = countBodyParams(paramList);
+		for (Map param : paramList) {
+			String destination = (String) param.get("destination");
+			String name = (String) param.get("name");
+			String destname = (String) param.get("destname");
+			if( isEmpty(destname)){
+				destname = name;
+			}	
+			Object def = param.get("defaultvalue");
+			Class type = this.types.get((String) param.get("type"));
+			Boolean opt = (Boolean) param.get("optional");
+			if ("property".equals(destination)) {
+				properties.put(destname, getValue(name, methodParams.get(name), def, opt, type));
+			} else if ("header".equals(destination)) {
+				headers.put(destname, getValue(name, methodParams.get(name), def, opt, type));
+			} else if ("body".equals(destination)) {
+				bodyObj = getValue(name, methodParams.get(name), def, opt, type);
+				bodyMap.put(destname, bodyObj);
+			}
+		}
+
+		if (bodyCount != 1) {
+			if (bodyMap.keySet().size() > 0) {
+				bodyObj = bodyMap;
+			} else {
+				bodyObj = null;
+			}
+		}
+		//properties.put("__logExceptionsOnly", getBoolean(shape, "logExceptionsOnly", false));
+		debug(this,"Consumer.prepare.methodParams:" + methodParams);
+		debug(this,"Consumer.prepare.paramList:" + paramList);
+		debug(this,"Consumer.prepare.properties:" + properties);
+		debug(this,"Consumer.prepare.headers:" + headers);
+		debug(this,"Consumer.prepare.body:" + bodyObj);
+
+		exchange.getIn().setBody(bodyObj);
+		exchange.getIn().setHeaders(headers);
+		if (properties != null) {
+			for (String key : properties.keySet()) {
+				exchange.setProperty(key, properties.get(key));
+			}
+		}
+	}
+
+	private void prepareExchange(Exchange exchange, PubSubData data) {
+		if (this.permissionService == null) {
+			this.permissionService = getByType(exchange.getContext(), PermissionService.class);
+		}
+		List<String> permittedRoleList = this.endpoint.getPermittedRoles();
+		List<String> permittedUserList = this.endpoint.getPermittedUsers();
+		String userName = getUserName();
+		List<String> userRoleList = getUserRoles(userName);
+		debug(this,"Consumer.prepare.userName:" + userName);
+		debug(this,"Consumer.prepare.userRoleList:" + userRoleList);
+		debug(this,"Consumer.prepare.permittedRoleList:" + permittedRoleList);
+		debug(this,"Consumer.prepare.permittedUserList:" + permittedUserList);
+		if (!isPermitted(userName, userRoleList, permittedUserList, permittedRoleList)) {
+			throw new RuntimeException(PERMISSION_DENIED + ":User(" + userName + ") has no permission");
+		}
+		exchange.getIn().setBody(objectMapper.convertValue( data.keywordArguments(), Map.class));
+	}
+	private Object getResult(Exchange exchange) {
+		String returnSpec = this.endpoint.getRpcReturn();
+		List<String> returnHeaderList = this.endpoint.getReturnHeaderList();
+		Object camelBody = ExchangeHelper.extractResultBody(exchange, null);
+		if ("body".equals(returnSpec)) {
+			return ExchangeHelper.extractResultBody(exchange, null);
+		} else if ("headers".equals(returnSpec)) {
+			Map<String, Object> camelVarMap = new HashMap();
+			for (Map.Entry<String, Object> header : exchange.getIn().getHeaders().entrySet()) {
+				if (returnHeaderList.size() == 0 || returnHeaderList.contains(header.getKey())) {
+					camelVarMap.put(header.getKey(), header.getValue());
+				}
+			}
+			return camelVarMap;
+		} else if ("bodyAndHeaders".equals(returnSpec)) {
+			Map<String, Object> camelVarMap = new HashMap();
+			if (camelBody instanceof Map<?, ?>) {
+				Map<?, ?> camelBodyMap = (Map<?, ?>) camelBody;
+				for (@SuppressWarnings("rawtypes")
+				Map.Entry e : camelBodyMap.entrySet()) {
+					if (e.getKey() instanceof String) {
+						camelVarMap.put((String) e.getKey(), e.getValue());
+					}
+				}
+			} else {
+				camelVarMap.put("body", camelBody);
+			}
+			for (Map.Entry<String, Object> header : exchange.getIn().getHeaders().entrySet()) {
+				if (returnHeaderList.size() == 0 || returnHeaderList.contains(header.getKey())) {
+					camelVarMap.put(header.getKey(), header.getValue());
+				}
+			}
+			return camelVarMap;
+		}
+		return null;
+	}
+
+	private ObjectNode buildResponse(final Object methodResult) {
+		ObjectNode node = null;
+		if( methodResult instanceof Map ){
+			node = this.objectMapper.valueToTree(methodResult);
+		}else{
+			node = this.objectMapper.createObjectNode();
+			node.putPOJO("result", methodResult);
+		}
+		return node;
+	}
+
+	private Map<String, Object> buildErrorResponse(final Exception exception) {
+		final Map<String, Object> error = new HashMap<String, Object>(6);
+		//error.put("origin", exception.getOrigin());
+		//error.put("code", exception.getErrorCode());
+		String cmessage = null;
+		Throwable cause = exception.getCause();
+		if (cause != null) {
+			while (cause.getCause() != null) {
+				cause = cause.getCause();
+			}
+			cmessage = cause.toString();
+			if (cmessage == null) {
+				cmessage = cause.toString();
+			}
+		}
+		error.put("message", constructMessage(exception.getMessage(), cmessage));
+		error.put("class", exception.getClass().getName());
+		return error;
+	}
+
+	private String constructMessage(String m1, String m2) {
+		if (m2 == null) {
+			return m1;
+		}
+		if (m1 != null && !m1.endsWith(":")) {
+			return m1 + ":" + m2;
+		}
+		return m1 + m2;
+	}
+
+	protected String getUserName() {
+		return ThreadContext.getThreadContext().getUserName();
+	}
+
+	protected boolean isPermitted(String userName, List<String> userRoleList, List<String> permittedUserList, List<String> permittedRoleList) {
+		if (permittedUserList.contains(userName)) {
+			return true;
+		}
+		for (String userRole : userRoleList) {
+			if (permittedRoleList.contains(userRole)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected List<String> getUserRoles(String userName) {
+		List<String> userRoleList = null;
+		try {
+			userRoleList = this.permissionService.getUserRoles(userName);
+		} catch (Exception e) {
+			userRoleList = new ArrayList<>();
+		}
+		return userRoleList;
+	}
+
+	protected Object getValue(String name, Object value, Object def, boolean optional, Class type) {
+		if (value == null && def != null) {
+			def = convertTo(def, type);
+			value = def;
+		}
+		if (value == null && optional == false) {
+			throw new RuntimeException("WampClientConsumer:Missing parameter:" + name);
+		}
+		if (value == null) {
+			return null;
+		}
+		if (!type.isAssignableFrom(value.getClass())) {
+			throw new RuntimeException("WampClientConsumer:parameter(" + name + ") wrong type:" + value.getClass() + " needed:" + type);
+		}
+		return value;
+	}
+
+	public static Object convertTo(Object sourceObject, Class<?> targetClass) {
+		try {
+			return ConvertUtils.convert(sourceObject, targetClass);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	protected int countBodyParams(List<Map> paramList) {
+		int count = 0;
+		for (Map param : paramList) {
+			String destination = (String) param.get("destination");
+			if ("body".equals(destination)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	private <T> T getByType(CamelContext ctx, Class<T> kls) {
+		return kls.cast(ctx.getRegistry().lookupByName(kls.getName()));
+	}
+
+	private boolean isEmpty(String s) {
+		return (s == null || "".equals(s.trim()));
+	}
+
+	protected void doStart() throws Exception {
+/* $if version >= 1.8 $ */
+		if( !this.endpoint.getMode().equals("rpc") && !this.endpoint.getMode().equals("subscribe")){
+			return;
+		}
+		super.doStart();
+		this.clientSession = endpoint.createWampClientSession(org.ms123.common.wamp.WampServiceImpl.DEFAULT_REALM);
+		info(this,"======Consumer.Start:" + this.clientSession);
+		this.clientSession.statusChanged().subscribe((state) -> {
+			info(this,"Consumer.ClientSession:status changed to " + state);
+			if (state == WampClientSession.Status.Connected) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+				}
+				wampClientConnected();
+			}
+			if (state == WampClientSession.Status.Disconnected) {
+			}
+		}, (t) -> {
+			debug(this,"Consumer.ClientSession ended with error " + t);
+			t.printStackTrace();
+		}, () -> {
+			debug(this,"Consumer.ClientSession ended normally");
+		});
+/* $endif$ */
+	}
+
+	protected void doStop() throws Exception {
+		String namespace = endpoint.getCamelContext().getName().split("/")[0];
+		//debug(this,"######Consumer.Stop:" + namespace + "." + endpoint.getProcedure() + "/" + this.hashCode());
+		this.clientSession.close();
+		super.doStop();
+	}
+}
+
