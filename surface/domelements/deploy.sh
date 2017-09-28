a4f4405a93fd4be9a21f5fffe7b1ee3466ae3f15
ms <manfred@ms123.org>
2017-09-28 08:13:10 +0200

    paper-fullscreen-dialog mv to src

--- surface/domelements/deploy.sh
+++ surface/domelements/deploy.sh
@@ -5,5 +5,6 @@ gzip -f domelements.html
 gzip -f domelements.js
 #scp domelements.html.gz  ms:$simpl4first/client/surface/
 #scp domelements.html.gz  vz5:$wawidev/client/surface/
-scp domelements.html.gz domelements.js.gz vz5:$flowabledev/client/surface/
+#scp domelements.html.gz domelements.js.gz vz5:$flowabledev/client/surface/
+scp domelements.html.gz domelements.js.gz vz5:$websitevz5/client/surface/
 scp domelements.html.gz domelements.js.gz vz5:$mbbeetkdev/client/surface/
